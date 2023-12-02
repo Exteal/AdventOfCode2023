92d988798ed168b153ecae65195e73505d7df868
Exteal <rh.4@laposte.net>
2023-12-02 14:08:59 +0100

    Day2

---
+++ src/Day02.kt
@@ -0,0 +1,100 @@
+import kotlin.math.max
+
+fun main() {
+    fun part1(input : List<String> ) : Int {
+        var correctSum = 0
+        for (game in input) {
+
+            val data =  game.split(':', ';')
+            val gameData = GameData(Integer.parseInt( data[0].filter { c -> c.isDigit() }))
+
+
+            val counts = data.drop(1)
+
+            counts.forEach { m -> m.split(",").forEach{
+                when {
+                    it.contains("red") -> {gameData.updateInfo(it, Colors.Red)}
+                    it.contains("green") -> {gameData.updateInfo(it, Colors.Green)}
+                    it.contains("blue") -> {gameData.updateInfo(it, Colors.Blue)}
+                }
+            }}
+
+            if (!GameData.hasError(gameData)) {
+                correctSum += gameData.getId()
+            }
+
+        }
+        return correctSum
+    }
+
+    fun part2(input : List<String>) : Int {
+        var setsCount = 0
+        for (game in input) {
+
+            val data =  game.split(':', ';')
+            val gameData = GameData(Integer.parseInt( data[0].filter { c -> c.isDigit() }))
+
+            val counts = data.drop(1)
+
+            counts.forEach {m -> m.split(",").forEach{
+                when {
+                    it.contains("red") -> {gameData.updateInfo(it, Colors.Red)}
+                    it.contains("green") -> {gameData.updateInfo(it, Colors.Green)}
+                    it.contains("blue") -> {gameData.updateInfo(it, Colors.Blue)}
+                }
+            }}
+
+            setsCount += gameData.setPower()
+
+        }
+        return setsCount
+    }
+
+    val input = readInput("Day02")
+
+
+    println(part1(input))
+    println(part2(input))
+}
+
+enum class Colors {
+    Red,
+    Green,
+    Blue
+}
+class GameData(private val id: Int) {
+    private var redCount = 0
+    private var blueCount = 0
+    private var greenCount = 0
+
+    companion object ErrorData {
+        private const val redMax = 12
+        private const val greenMax = 13
+        private const val blueMax = 14
+
+        fun hasError(gameData: GameData) : Boolean{
+            return gameData.redCount > redMax || gameData.greenCount > greenMax || gameData.blueCount > blueMax
+        }
+    }
+
+    fun getId() : Int {
+        return id
+    }
+    fun updateInfo(info: String, color : Colors) {
+       val count =  Integer.parseInt( info.filter { c -> c.isDigit() })
+        when(color) {
+            Colors.Red -> { redCount = max(redCount, count) }
+            Colors.Green -> { greenCount = max(greenCount, count) }
+            Colors.Blue -> { blueCount = max(blueCount, count) }
+        }
+
+    }
+
+    override fun toString(): String {
+        return "red :$redCount green :$greenCount blue : $blueCount \n"
+    }
+
+    fun setPower() : Int {
+        return redCount * greenCount * blueCount
+    }
+}
